3b256c1fc196fcb1f00a62c18a3a4fdc77a0ec2b
dawnzzhang <1071102546@qq.com>
2024-01-24 17:39:29 +0800

    upload figures

--- run_recbole.py
+++ run_recbole.py
@@ -14,6 +14,6 @@ if __name__ == '__main__':
     args, _ = parser.parse_known_args()
     wandb.init(
         name='exp-' + args.dataset + '-' + args.model,
-        project="SSL4Rec")
+        project="RecDCL")
     config_file_list = args.config_files.strip().split(' ') if args.config_files else None
     run_recbole(model=args.model, dataset=args.dataset, config_file_list=config_file_list)
